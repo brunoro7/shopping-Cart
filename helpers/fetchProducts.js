ebbfeb37933dd1440c70d531d9827baa700cd22d
brunoro7 <bruno.ro7c@gmail.com>
2022-02-04 01:28:29 -0300

    apenas registrando progresso até o momento, ainda trabalhando no requisito 1, usando o 8 como tdd.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,10 +1,22 @@
-const fetchProducts = () => {
-  console.log('oi');
-};
+function objProduct(dataJson) {
+  return {
+    sku: dataJson.results[0].id,
+    name: dataJson.results[0].title,
+    image: dataJson.results[0].thumbnail,
+  };
+}
 
+const fetchProducts = async (searchValue) => {
+  const apiResponse = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${searchValue}`);
+  const dataJson = await apiResponse.json();
+  const product = objProduct(dataJson);
+
+  console.log(product);
+  return dataJson;
+};
+fetchProducts('computador');
 if (typeof module !== 'undefined') {
   module.exports = {
     fetchProducts,
   };
 }
-fetchProducts();
